b48606864f8e8ac65d6cacc2ce41505725fcd5d9
Jay C <james.carpentera3@gmail.com>
2015-04-01 16:03:37 +0000

    Adding dropbox support to carrierwave

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,8 @@
+CarrierWave.configure do |config|
+  config.dropbox_app_key = "durpz9n4gn7i12f"
+  config.dropbox_app_secret = "dreawr4g5hqlxn4"
+  config.dropbox_access_token = "4d7vh681cve6qbu0"
+  config.dropbox_access_token_secret = "khn1wy4gxhbx6fj"
+  config.dropbox_user_id = "134513580"
+  config.dropbox_access_type = "app_folder"
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150327023424) do
+ActiveRecord::Schema.define(version: 20150401150040) do
 
   create_table "listings", force: true do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20150327023424) do
     t.decimal  "price"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image"
   end
 
 end
